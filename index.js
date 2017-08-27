da9354ea8c7bfd814aaa8b62a01d5150256fed0f
Thiago Dias <thiagodiaz7@gmail.com>
2017-08-27 13:19:05 -0300

    Adicionando template de Lista na resposta se o usuário digitar List

--- index.js
+++ index.js
@@ -48,12 +48,19 @@ app.post('/webhook/', function (req, res) {
 
         if (event.message && event.message.text) {
             text = event.message.text
+            if (text === 'List') {
+                sendListMessage(sender)
+                continue
+            }
+        }
+
+        if (event.message && event.message.text) {
+            text = event.message.text
             if (text === 'Button') {
                 sendButtonMessage(sender)
                 continue
             }
             sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
-
         }
 
         if (event.postback) {
@@ -68,6 +75,7 @@ app.post('/webhook/', function (req, res) {
 let token = "EAABqgKLBCIMBADt70pPRVbZCT8JgZBBzgnRVqWCcAsYU7Guht95M7M4VZAJrZBuDrNhaZCzGcQpTx0NrrFL0CZBjJxtYeruegCS7Bjrfe10Xk9xXaTRFFjhJkIZCrO0HeQFCZCkwy8ZC6SwKQPLsJeDIDNpkACTb0cfF0E8Eet7iedCReWeUGxdQ8"
 
 
+// Templates de Resposta
 function sendTextMessage(sender, text) {
     messageData = {
         text:text
@@ -88,6 +96,7 @@ function sendTextMessage(sender, text) {
         }
     })
 }
+
 
 
 function sendGenericMessage(sender) {
@@ -120,6 +129,90 @@ function sendGenericMessage(sender) {
                     }],
                 }]
             }
+        }
+    }
+    request({
+        url: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: {access_token:token},
+        method: 'POST',
+        json: {
+            recipient: {id:sender},
+            message: messageData,
+        }
+    }, function(error, response, body) {
+        if (error) {
+            console.log('Error sending messages: ', error)
+        } else if (response.body.error) {
+            console.log('Error: ', response.body.error)
+        }
+    })
+}
+
+function sendListMessage(sender) {
+    messageData = {
+        "attachment": {
+          "type": "template",
+          "payload": {
+            "template_type": "list",
+            "top_element_style": "compact",
+            "elements": [
+              {
+                "title": "Classic T-Shirt Collection",
+                "subtitle": "See all our colors",
+                "image_url": "https://peterssendreceiveapp.ngrok.io/img/collection.png",          
+                "buttons": [
+                  {
+                    "title": "View",
+                    "type": "web_url",
+                    "url": "https://peterssendreceiveapp.ngrok.io/collection",
+                    "messenger_extensions": true,
+                    "webview_height_ratio": "tall",
+                    "fallback_url": "https://peterssendreceiveapp.ngrok.io/"            
+                  }
+                ]
+              },
+              {
+                "title": "Classic White T-Shirt",
+                "subtitle": "See all our colors",
+                "default_action": {
+                  "type": "web_url",
+                  "url": "https://peterssendreceiveapp.ngrok.io/view?item=100",
+                  "messenger_extensions": true,
+                  "webview_height_ratio": "tall",
+                  "fallback_url": "https://peterssendreceiveapp.ngrok.io/"
+                }
+              },
+              {
+                "title": "Classic Blue T-Shirt",
+                "image_url": "https://peterssendreceiveapp.ngrok.io/img/blue-t-shirt.png",
+                "subtitle": "100% Cotton, 200% Comfortable",
+                "default_action": {
+                  "type": "web_url",
+                  "url": "https://peterssendreceiveapp.ngrok.io/view?item=101",
+                  "messenger_extensions": true,
+                  "webview_height_ratio": "tall",
+                  "fallback_url": "https://peterssendreceiveapp.ngrok.io/"
+                },
+                "buttons": [
+                  {
+                    "title": "Shop Now",
+                    "type": "web_url",
+                    "url": "https://peterssendreceiveapp.ngrok.io/shop?item=101",
+                    "messenger_extensions": true,
+                    "webview_height_ratio": "tall",
+                    "fallback_url": "https://peterssendreceiveapp.ngrok.io/"            
+                  }
+                ]        
+              }
+            ],
+             "buttons": [
+              {
+                "title": "View More",
+                "type": "postback",
+                "payload": "payload"            
+              }
+            ]  
+          }
         }
     }
     request({
