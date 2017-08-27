d1a2c003b00880c4a0b593d39a44e9daffa1ad68
Thiago Dias <thiagodiaz7@gmail.com>
2017-08-27 13:12:20 -0300

    Adicionando mais uma mensagem e enviando uma resposta de um template do tipo Button

--- index.js
+++ index.js
@@ -16,6 +16,8 @@ app.get('/', function (req, res) {
     res.send('Hello world, I am a chat bot')
 })
 
+
+
 // for Facebook verification
 app.get('/webhook/', function (req, res) {
     if (req.query['hub.verify_token'] === '912193bd-3ccc-46c7-a416-e12c32403874') {
@@ -44,6 +46,15 @@ app.post('/webhook/', function (req, res) {
             }
             sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
         }
+
+        if (event.message && event.message.text) {
+            text = event.message.text
+            if (text === 'Button') {
+                sendButtonMessage(sender)
+                continue
+            }
+        }
+
         if (event.postback) {
             text = JSON.stringify(event.postback)
             sendTextMessage(sender, "Postback received: "+text.substring(0, 200), token)
@@ -53,7 +64,7 @@ app.post('/webhook/', function (req, res) {
     res.sendStatus(200)
 })
 
-var token = "EAABqgKLBCIMBADt70pPRVbZCT8JgZBBzgnRVqWCcAsYU7Guht95M7M4VZAJrZBuDrNhaZCzGcQpTx0NrrFL0CZBjJxtYeruegCS7Bjrfe10Xk9xXaTRFFjhJkIZCrO0HeQFCZCkwy8ZC6SwKQPLsJeDIDNpkACTb0cfF0E8Eet7iedCReWeUGxdQ8"
+let token = "EAABqgKLBCIMBADt70pPRVbZCT8JgZBBzgnRVqWCcAsYU7Guht95M7M4VZAJrZBuDrNhaZCzGcQpTx0NrrFL0CZBjJxtYeruegCS7Bjrfe10Xk9xXaTRFFjhJkIZCrO0HeQFCZCkwy8ZC6SwKQPLsJeDIDNpkACTb0cfF0E8Eet7iedCReWeUGxdQ8"
 
 
 function sendTextMessage(sender, text) {
@@ -108,6 +119,47 @@ function sendGenericMessage(sender) {
                     }],
                 }]
             }
+        }
+    }
+    request({
+        url: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: {access_token:token},
+        method: 'POST',
+        json: {
+            recipient: {id:sender},
+            message: messageData,
+        }
+    }, function(error, response, body) {
+        if (error) {
+            console.log('Error sending messages: ', error)
+        } else if (response.body.error) {
+            console.log('Error: ', response.body.error)
+        }
+    })
+}
+
+
+
+function sendButtonMessage(sender) {
+    messageData = {
+        "attachment":{
+          "type":"template",
+          "payload":{
+            "template_type":"button",
+            "text":"What do you want to do next?",
+            "buttons":[
+              {
+                "type":"web_url",
+                "url":"https://www.messenger.com",
+                "title":"Visit Messenger"
+              },
+              {
+                "type":"web_url",
+                "url":"https://www.messenger.com",
+                "title":"Visit Messenger"
+              }
+            ]
+          }
         }
     }
     request({
